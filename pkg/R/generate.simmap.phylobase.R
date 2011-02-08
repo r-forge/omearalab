8611c0bd6063e0b85c3fac297657a0bf58e404f4
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-02-08 21:31:23 +0000

    new implementation using phylobase
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/generate.simmap.phylobase.R
@@ -0,0 +1,40 @@
+library(phylobase)
+generate.simmap<-function(x, taxa.vector, change.position=0.5, digits=10, suppress.zero=FALSE) {
+	f.d <- paste("%.", digits, "g", sep = "") 
+	x.reorder<-reorder(x, "postorder")
+	description.vector<-rep(NA,nEdges(x.reorder))
+	mrcaNode<-MRCA(x.reorder,taxa.vector)
+	for (edgeIndex in 1:nEdges(x.reorder)) {
+		currentEdge<-edges(x.reorder)[edgeIndex,2]
+		currentNode<-getNode(x.reorder,currentEdge)
+		state=0
+		if (currentNode==mrcaNode) {
+			state=-1	
+		}
+		else if (length(which(descendants(x.reorder,mrcaNode,type="all")==currentNode))==1) { #the current node is in the set of descendants of the mrca node
+			state=1
+		}
+		simmapLabel=""
+		if (rootNode(x.reorder)!=currentNode) { #don't do this for the root node
+			if (state!=-1) {
+				simmapLabel=paste(":{",state,",",sprintf(f.d, edgeLength(x.reorder,currentNode)),"}",sep="")
+			}
+			else {
+				if ((change.position>0 && change.position<1) || !suppress.zero) { #so, if we don't have to worry about trimming parts with zero state
+					simmapLabel=paste(":{1,",sprintf(f.d, (1-change.position)*edgeLength(x.reorder,currentNode)),":0,",sprintf(f.d, change.position*edgeLength(x.reorder,currentNode)),"}",sep="")
+				}
+				else {
+					if(change.position==0) { #suppress.zero must be true to get this far
+						simmapLabel=paste(":{1,",sprintf(f.d, edgeLength(x.reorder,currentNode)),"}",sep="")
+					}	
+					else { #change.position must be 1 and suppress.zero==true
+						simmapLabel=paste(":{0,",sprintf(f.d, edgeLength(x.reorder,currentNode)),"}",sep="")
+					}
+				}
+			}
+		}
+		if(nodeType(getNode(x.reorder,currentEdge))=="tip") {
+			
+		}
+	}
+}
