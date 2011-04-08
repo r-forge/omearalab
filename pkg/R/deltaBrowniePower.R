5c0721383ec689408444e36e6df7bece80c74932
halamillo (Hugo Alamillo) <alamillo@utk.edu>
2011-04-08 18:30:32 +0000

    deltaBrownie.R now includes generate.simmap() and I have altered  deltaBrowniePower.r to the point where Brian and I can test it on the cluster
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/deltaBrowniePower.R
+++ pkg/R/deltaBrowniePower.R
@@ -1,13 +1,13 @@
+setwd(/data/cichlids/)
+
 ntax.vector=c(2^4, 2^5, 2^6, 2^7, 2^8)
 shape.vector=c("balanced", "right")
 rate.vector=c(0.1, 0.5, 1, 2, 10)
 change.position.vector=c("root", "quarter", "cherry")
-reps.per.combination=10
+reps.per.combination=1
 
-data = runif((nTips(phy)), 1, 10)
-#runif(n, min=0, max=1)
 
-setwd(/data/cichlids/)
+
 
 
 for(rep in 1:reps.per.combination){
@@ -23,33 +23,38 @@ for(rep in 1:reps.per.combination){
 				for(change.position.index in 1:length(change.position.vector)){
 					change=change.position.vector[change.position.index]
 					
-					fileNameRoot<-paste("ntax",ntax,"shape",shape,"rate",rate,"change",change,"rep",rep,sep="",collapse="")
+					fileNameRoot<-paste("ntax.",ntax,".shape.",shape,".rate.",rate,".change.",change,".rep.",rep,sep="",collapse="")
 					batchFileName<-paste(fileNameRoot,".R",sep="",collapse="")
 					cat("library(geiger)\n","library(RBrownie)\n",sep="",file=batchFileName,append=FALSE)
 					cat("ntax=",ntax,"\nshape='",shape,"'\nrate=",rate,"\nchange='",change,"'\nrep=",rep,"\n",sep="",file=batchFileName,append=TRUE)
 					cat("source('deltaBrownie.R')","\n",sep="",file=batchFileName,append=TRUE)
 					cat("streeBrlen(ntax, type=shape)->phy\n",sep="",file=batchFileName,append=TRUE)
+					cat("blMultiplier(phy, rate, change, shape )->phy1\n",sep="",file=batchFileName,append=TRUE )
+					cat("model.matrix<-matrix(1)","\n", sep="",file=batchFileName,append=TRUE )
+					cat("data<-sim.char(phy1, model.matrix, nsims = 1, model = 'brownian', root.state = 1 )", "\n",sep="",file=batchFileName,append=TRUE)
+					cat("data<-c(data[,,1])", "\n",sep="",file=batchFileName,append=TRUE)
+					cat("iterateNonCensored(phy, data)->results.",fileNameRoot, "\n",sep="",file=batchFileName,append=TRUE)
+					cat("save(results.",fileNameRoot, ', file=paste(results.',fileNameRoot,".data.frame",',sep="", ascii=TRUE, compress=FALSE)\n', sep="",file= batchFileName, append=TRUE) 
+										
+					#for plotting the pdf:
 					
-					cat("data = runif((nTips(phy)), 1, 10)\n",sep="",file=batchFileName,append=TRUE )
-					
-					#look at Barb's code to plot to a pdf each time for the transformed tree
-					
+					cat("pdf(paste('phy1',", fileNameRoot,'.pdf, sep="")', "\n", sep="",file=batchFileName,append=TRUE)
+					cat("plot(phy1)", "\n",sep="",file=batchFileName,append=TRUE)
+					cat("title(main=", fileNameRoot, ', col.main="red", font.main=2)', "\n",sep="",file=batchFileName,append=TRUE)
+					cat("dev.off()", "\n",sep="",file=batchFileName,append=TRUE)
 					
 					#TO DO: KEEP ADDING COMMANDS FOR THE BATCH FILE 
 						#where is the phy that streeBrlen is outputting with each file (i.e. ntax256shaperightrate10changecherryrep8.R), this object needs to be fed in the write() below marked "HERE!"
-						#each one of the results from each (ntax256shaperightrate10changecherryrep8.R) file needs to fed throgh iterateNonCensored()
-						#to do the iterateNonCensored will need data associated with each tree
 						
 						
 						
 						
-					#MAKE SURE EACH DATA.FRAME IS SAVED AS AN R OBJECT IN ANOTHER FILE
-					#write(HERE!, file=paste("run", HERE!, sep="")) 
+					#MAKE SURE EACH DATA.FRAME IS SAVED AS AN R OBJECT IN ANOTHER FILE DONW
 					
-					#TO DO: SYSTEM(EZSUB R CMD BATCH BATCHFILENAME)
+					#TO DO: SYSTEM(EZSUB R CMD BATCH BATCHFILENAME)  DONE
 					#intern=TRUE captures the command as an R character vector; all other TRUE/FALSE arguements are left with their defaults, except for the command argument wich pastes the directory
-					#system(command=paste("/home/alamillo/bin/ezsubmediumR R CMD BATCH ", batchFileName, sep=""), intern=TRUE, ignore.stderr=FALSE, wait=TRUE, input=NULL, show.output.on.console=FALSE)
-					#Sys.sleep(5)
+					system(command=paste("/home/alamillo/bin/ezsubmediumR R CMD BATCH ", batchFileName, sep=""), intern=TRUE, ignore.stderr=FALSE, wait=TRUE, input=NULL, show.output.on.console=FALSE)
+					Sys.sleep(5)
 					
 				}
 			}
@@ -57,15 +62,20 @@ for(rep in 1:reps.per.combination){
 	}	
 }
 
-for plotting the pdf:
-if (savePlot) {
-               pdf(paste("SimTree", jobName, ".pdf", sep=""))        
-               plot(x=c(min(c(startVector, endVector)), max(c(startVector, endVector))), y=c(0, max(c(startTime, endTime))), type="n", ylab="Time", xlab="Trait value", main="", bty="n")
-               for (i in 1:length(startVector)) {
-                       lines(x=c(startVector[i], endVector[i]), y=max(c(startTime, endTime)) - c(startTime[i], endTime[i]))
-               }
-               dev.off()
-       }
+#for plotting the pdf:
+#if (savePlot) {
+				#pdf(paste("phy", fileNameRoot, ".pdf", sep=""))
+				#plot(phy3)
+				#title(main=fileNameRoot, col.main="red", font.main=2)
+				#dev.off()
+
+        
+               #plot(x=c(min(c(startVector, endVector)), max(c(startVector, endVector))), y=c(0, max(c(startTime, endTime))), type="n", ylab="Time", xlab="Trait value", main="", bty="n")
+               #for (i in 1:length(startVector)) {
+                      # lines(x=c(startVector[i], endVector[i]), y=max(c(startTime, endTime)) - c(startTime[i], endTime[i]))
+              # }
+               #dev.off()
+       #}
 
 
 
