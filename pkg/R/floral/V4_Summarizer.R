4673f8590e70c2d9fd518e00658a4d75ee47a652
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-05-03 21:18:52 +0000

    removed redundant rename step if there's already a summary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/floral/V4_Summarizer.R
+++ pkg/R/floral/V4_Summarizer.R
@@ -76,25 +76,30 @@ while(1<2) { #this will keep looping, updating the summary
 	summary.dataframe<-cbind(deltaAIC,AICweight,summary.dataframe)
 	
 	#now time to make nice names for things
-	for (charStateI in 1:((2^S))) { 
-		binaryStateIVector<-digitsBase(charStateI-1,ndigits=S)[,1]
-		iLabelShort<-sprintf(paste("%0",maxStringLength,"d",sep=""),charStateI)
-		iLabelLong<-vectorToString(binaryStateIVector)
-		names(summary.dataframe)[which(names(summary.dataframe) == paste("lambda",iLabelShort,sep="",collapse=""))]<-paste("lambda",iLabelLong,sep="",collapse="")
-		names(summary.dataframe)[which(names(summary.dataframe) == paste("mu",iLabelShort,sep="",collapse=""))]<-paste("mu",iLabelLong,sep="",collapse="")
-		print(paste("changing names for ",iLabelLong))
-		for (charStateJ in 1:((2^S))) { 
-			binaryStateJVector<-digitsBase(charStateJ-1,ndigits=S)[,1]
-			jLabelShort<-sprintf(paste("%0",maxStringLength,"d",sep=""),charStateJ)
-			jLabelLong<-vectorToString(binaryStateJVector)
-			numberMismatches=vectorMismatch(binaryStateIVector,binaryStateJVector) #so we have two vectors, say 00101 and 00110 (though as length 5 vectors). Doing v1==v2 leads to T T T F F. T=1 for R and F=0, so 1-(v1==v2) = c(1-1,1-1,1-1,1-0,1-0), sum of which is the number of mismatches
-			if (numberMismatches==1) {
-				names(summary.dataframe)[which(names(summary.dataframe) == paste("q",iLabelShort,jLabelShort,sep="",collapse=""))]<-paste("q",iLabelLong,"_",jLabelLong,sep="",collapse="")			
-			}
-			else {
-				names(summary.dataframe)[which(names(summary.dataframe) == paste("q",iLabelShort,jLabelShort,sep="",collapse=""))]<-paste("q",iLabelLong,"_",jLabelLong,"_disallowed",sep="",collapse="")			
+	if (loadedOld==FALSE) {
+		for (charStateI in 1:((2^S))) { 
+			binaryStateIVector<-digitsBase(charStateI-1,ndigits=S)[,1]
+			iLabelShort<-sprintf(paste("%0",maxStringLength,"d",sep=""),charStateI)
+			iLabelLong<-vectorToString(binaryStateIVector)
+			names(summary.dataframe)[which(names(summary.dataframe) == paste("lambda",iLabelShort,sep="",collapse=""))]<-paste("lambda",iLabelLong,sep="",collapse="")
+			names(summary.dataframe)[which(names(summary.dataframe) == paste("mu",iLabelShort,sep="",collapse=""))]<-paste("mu",iLabelLong,sep="",collapse="")
+			print(paste("changing names for ",iLabelLong))
+			for (charStateJ in 1:((2^S))) { 
+				binaryStateJVector<-digitsBase(charStateJ-1,ndigits=S)[,1]
+				jLabelShort<-sprintf(paste("%0",maxStringLength,"d",sep=""),charStateJ)
+				jLabelLong<-vectorToString(binaryStateJVector)
+				numberMismatches=vectorMismatch(binaryStateIVector,binaryStateJVector) #so we have two vectors, say 00101 and 00110 (though as length 5 vectors). Doing v1==v2 leads to T T T F F. T=1 for R and F=0, so 1-(v1==v2) = c(1-1,1-1,1-1,1-0,1-0), sum of which is the number of mismatches
+				if (numberMismatches==1) {
+					names(summary.dataframe)[which(names(summary.dataframe) == paste("q",iLabelShort,jLabelShort,sep="",collapse=""))]<-paste("q",iLabelLong,"_",jLabelLong,sep="",collapse="")			
+				}
+				else {
+					names(summary.dataframe)[which(names(summary.dataframe) == paste("q",iLabelShort,jLabelShort,sep="",collapse=""))]<-paste("q",iLabelLong,"_",jLabelLong,"_disallowed",sep="",collapse="")			
+				}
 			}
 		}
+	}
+	else {
+		names(summary.dataframe)<-names(old.summary.dataframe)
 	}
 
 	system("cp ../Summaries/RateSummary.txt ../Summaries/PreviousRateSummary.txt")
