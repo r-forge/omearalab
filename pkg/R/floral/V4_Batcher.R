922aec9a6dad29e5e036275f8626b2e9871bc73e
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-05-05 14:35:54 +0000

    updated batcher
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/floral/V4_Batcher.R
+++ pkg/R/floral/V4_Batcher.R
@@ -9,6 +9,7 @@ focalVectorList<-getAllInterestingFocalVectorsStringsEfficient(S)
 
 totalRuns<-0
 runsInFile<-0
+pbsCommands=""
 
 for (focalIndex in 1:length(focalVectorList)) {
 	focalVector<-stringToVector(unlist(focalVectorList[[focalIndex]]))
@@ -22,13 +23,12 @@ for (focalIndex in 1:length(focalVectorList)) {
 				if(numberFocalCombos(focalVector) >= diversificationModels$min_focalcombos[diversificationModelIndex]) { #if there aren't enough combos to make the model appropriate, don't run it
 					totalRuns<-totalRuns+1
 					#yay! Now we can run!
-					Sys.sleep(1)
 					nameRoot<-paste("T",transitionModelIndex,"_D",diversificationModelIndex,"_",vectorToString(getFocalSummaryLabel(focalVector,S,"x")),sep="",collapse="")
 					dirRoot<-paste("/data/abc/RunsApril2011/ActualRuns/T",transitionModelIndex,"/T",transitionModelIndex,"_D",diversificationModelIndex,"/",nameRoot,sep="",collapse="")
 					mkdirCmd=paste("mkdir -p ",dirRoot,sep="",collapse="")
 					suppressWarnings(system(mkdirCmd))
 					lsString=paste(paste("ls -1 ",dirRoot,' | grep -c final.matrix.all',sep="",collapse=""))
-					print(lsString)
+					#print(lsString)
 					finalMatrixAllCount=suppressWarnings(as.numeric(system(lsString,intern=TRUE)))
 					if(finalMatrixAllCount==0) {
 						runCommand=paste("source('/data/abc/RunsApril2011/UnifiedApproachScripts/V4_Commands.R')\ndoUnifiedRun(F='",vectorToString(focalVector),"',T=",transitionModelIndex,",D=",diversificationModelIndex,",S=",partitionSize,")",sep="",collapse="")
@@ -52,7 +52,8 @@ for (focalIndex in 1:length(focalVectorList)) {
 						#print(paste(paste("../ActualRuns/P",partitionSchemeText,sep="",collapse=""),"/",nameRoot,'/run.sh',sep=""))
 						pbsCommands=paste(pbsCommands,"\nrm ",' *.csv *.t ',sep="")
 						runsInFile=runsInFile+1
-						if (runsInFile>4) { #change this to deal with remnants
+						print(paste("Queuing run ",nameRoot," at ",date(),sep="",collapse=""))
+						if (runsInFile>1) { #change this to deal with remnants
 							cat(pbsCommands,file=paste(dirRoot,'/run.sh',sep=""),append=FALSE)
 							print(pbsCommands)
 							#print(paste("cd ",paste("../ActualRuns/P",partitionSchemeText,sep="",collapse=""),"/",nameRoot,sep=""))
@@ -62,11 +63,11 @@ for (focalIndex in 1:length(focalVectorList)) {
 							system("chmod u+x run.sh")
 							system("qsub run.sh")
 							setwd(origWD)
-							Sys.sleep(1)
+							Sys.sleep(10)
 							runsInFile=0
 							pbsCommands=""
 						}
-						while(as.numeric(system("qstat | grep -c bomeara",intern=TRUE))>500) {
+						while(as.numeric(system("qstat | grep -c bomeara",intern=TRUE))>300) {
 							Sys.sleep(117)
 						}
 					}			
