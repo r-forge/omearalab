fb26794ab12c3c36d5226dea655c28b52730e695
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-05-31 19:11:55 +0000

    HighLevelSummary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/floral/HighLevelSummary.R
@@ -0,0 +1,25 @@
+load("V4_UtilityFns.R")
+setwd("/Users/bomeara/Sites/Floral/RunsApril2011/Summaries")
+highlevel.dataframe<-data.frame()
+for (T in 1:5) {
+	for (D in 1:6) {
+		load(paste("RateSummaryT",T,"D",D,".Rsave"))
+		if (T+D==2) { #first one
+			highlevel.dataframe<-summary.dataframe[,3:13]
+		}
+		else {
+			highlevel.dataframe<-rbind(highlevel.dataframe,summary.dataframe[,3:13])
+		}
+		save(highlevel.dataframe,file="Highlevel.dataframe.Rsave",compress=TRUE)
+		print(paste("just did  T",T,"D",D,"with length =",dim(highlevel.dataframe)[1]))
+	}
+}
+
+rm(summary.dataframe)
+highlevel.dataframe->summary.dataframe
+rm(highlevel.dataframe)
+deltaAIC<-summary.dataframe$AIC-min(summary.dataframe$AIC)
+relativeLikelihood<-exp(-0.5 * deltaAIC)
+AICweight<-relativeLikelihood/sum(relativeLikelihood)
+summary.dataframe<-cbind(deltaAIC,AICweight,summary.dataframe)
+summarizeModelWeights(summary.dataframe,S)
