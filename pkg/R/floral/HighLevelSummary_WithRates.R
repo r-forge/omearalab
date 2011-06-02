1f7977a68edfa5b1efc788fadeab939ca43204ab
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-06-02 14:52:39 +0000

    Now with more rates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/floral/HighLevelSummary_WithRates.R
@@ -0,0 +1,25 @@
+source("V4_UtilityFns.R")
+setwd("/Users/bomeara/Sites/Floral/RunsApril2011/Summaries")
+ls()
+highlevel.dataframe<-data.frame()
+for (T in 1:5) {
+	for (D in 1:6) {
+		load(paste("RateSummaryT",T,"D",D,".Rsave"))
+		if (T+D==2) { #first one
+			highlevel.dataframe<-summary.dataframe[,3:13]
+		}
+		else {
+			highlevel.dataframe<-rbind(highlevel.dataframe,summary.dataframe[,3:(dim(summary.dataframe)[2])])
+		}
+		save(highlevel.dataframe,file="/Users/bomeara/Sites/Floral/RunsApril2011/Summaries/Highlevel.dataframe.withrates.Rsave",compress=TRUE)
+		print(paste("just did  T",T,"D",D,"with length =",dim(highlevel.dataframe)[1]))
+	}
+}
+
+ls()
+deltaAIC<-highlevel.dataframe$AIC-min(highlevel.dataframe$AIC)
+relativeLikelihood<-exp(-0.5 * deltaAIC)
+AICweight<-relativeLikelihood/sum(relativeLikelihood)
+highlevel.dataframe<-cbind(deltaAIC,AICweight,highlevel.dataframe)
+save(highlevel.dataframe,file="/Users/bomeara/Sites/Floral/RunsApril2011/Summaries/Highlevel.dataframe.withrates.Rsave",compress=TRUE)
+summarizeModelWeights(summary.dataframe=highlevel.dataframe,S=S,transitionModels=transitionModels, diversificationModels=diversificationModels)
