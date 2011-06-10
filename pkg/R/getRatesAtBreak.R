277566f9284cba53497692881224be8dbfd0d290
halamillo (Hugo Alamillo) <alamillo@utk.edu>
2011-06-10 21:47:50 +0000

    getRatesAtBreak.R now can calculate the model averaged rates from a deltaBrownie object
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@138 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getRatesAtBreak.R
+++ pkg/R/getRatesAtBreak.R
@@ -1,11 +1,13 @@
 #tip.label<-c("Bipes_biporus", "Bipes_cannaliculatus", "Bipes_tridactylus", "Bipes_alvarezi", "Bipes_sp")
-#tree1<-rcoal(5, tip.label)> 
+#tree1<-rcoal(5, tip.label)
 #data = runif((nTips(tree1)), 1, 10)
 #data<-as.data.frame(data)
 #rownames(data)<-tip.label
 #bipes.results<-iterateNonCensored(tree1, data)
 #bipes.results$tree.with.shift->phyext_tree
 #plot(phyext_tree)
+
+
 
 getRatesAtBreak<-function(phy, BMS.node) {
 
@@ -29,12 +31,15 @@ getRatesAtBreak<-function(phy, BMS.node) {
 	
 		}
 	colnames(rate.choice)<-c("pre", "post")	
-	cat(rate.choice)
+	#cat(rate.choice)
 	#rate.choice.pre<-append(rate.choice.pre, rate.choice, after=length(rate.choice.pre))
 		
 	}	
 	return(rate.choice)	
 }	
+
+
+result.iterateNonCensored<-bipes.results
 
 
 #Stuff that is R but iscommented out is to fix
@@ -43,25 +48,40 @@ getModelAveragedRates<-function(phy, result.iterateNonCensored) {
 	nodeList<-nodeId(phy,type=c("all"))	
 	rate.matrix<-matrix(data=0, nrow=length(nodeList), ncol=2)  #this is where we will stick the model-averaged rates
 
-	for(modelIndex in 1:dim(result.iterateNonCensored)[1]) {
+	for(modelIndex in 1:length(result.iterateNonCensored$all.test.results$NodeShift)) { #this goes by row and NodeShift does not match the order of the rows see model.rates[3]
+		
 		model.rates<-rep(NA,2)
-		model.rates[1]<-#result.iterateNonCensored$rate0[modelIndex] #CHECK THIS SYNTAX, make sure it works for BM1, too
-		model.rates[2]<-#result.iterateNonCensored$rate1[modelIndex] #ditto
-		AICweight<-#result.iterateNonCensored$AICweight[modelIndex]
-		BMS.node<-#result.iterateNonCensored#BMS.node[modelIndex]
+		
+		model.rates[1]<-result.iterateNonCensored$all.test.results$Rate_in_state_0[modelIndex] #CHECK THIS SYNTAX, make sure it works for BM1, too
+		
+		model.rates[2]<-result.iterateNonCensored$all.test.results$Rate_in_state_1[modelIndex] #ditto
+		
+		#model.rates[3]<-result.iterateNonCensored$all.test.results$NodeShift[modelIndex] #gives back value of node shift not the row 
+
+
+		AICcweight<-result.iterateNonCensored$all.test.results$AICcweight[modelIndex]
+		
+		
+		BMS.node<-result.iterateNonCensored$all.test.results$NodeShift[modelIndex] #does this need to be the NodeShift column?
+		#if(result.iterateNonCensored$all.test.results$NodeShift[modelIndex]!="NA") {
+		#	BMS.node<-result.iterateNonCensored$all.test.results$NodeShift[modelIndex]
+		#}	
+		
 		rate.choice<-matrix(data=NA, nrow=length(nodeList), ncol=2)
-		if(result.iterateNonCensored$model[modelIndex]=="BM1") {
+		if(result.iterateNonCensored$all.test.results$Model[modelIndex]=="BM1") {
 			rate.choice<-matrix(data=0, nrow=length(nodeList), ncol=2)
 		}
 		else {
 			rate.choice<-getRatesAtBreak(phy,BMS.node)
 		}
+		
 		for(nodeIndex in 1:length(nodeList)){
 			each.node<-nodeList[nodeIndex]
 			for (position in 1:2) {
-				rate.matrix[nodeIndex,position] <- rate.matrix[nodeIndex,1] + ( AICweight * model.rates[1+ rate.choice[nodeIndex,position] ] )
+				rate.matrix[nodeIndex,position] <- rate.matrix[nodeIndex,position] + ( AICcweight * model.rates[1+ rate.choice[nodeIndex,position] ] )
 			}
 		}
 	}
+	rownames(rate.matrix)<-nodeList
 	return(rate.matrix)
 }
