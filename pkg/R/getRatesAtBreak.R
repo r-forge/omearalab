6b0cbf9fdf23c2eda1995b48ea49e43b9c7759c2
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-06-10 15:30:40 +0000

    added pseudocode for model averaged rates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@137 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getRatesAtBreak.R
+++ pkg/R/getRatesAtBreak.R
@@ -33,5 +33,35 @@ getRatesAtBreak<-function(phy, BMS.node) {
 	#rate.choice.pre<-append(rate.choice.pre, rate.choice, after=length(rate.choice.pre))
 		
 	}	
-return(rate.choice)	
+	return(rate.choice)	
 }	
+
+
+#Stuff that is R but iscommented out is to fix
+getModelAveragedRates<-function(phy, result.iterateNonCensored) {
+	node.vector<-c()
+	nodeList<-nodeId(phy,type=c("all"))	
+	rate.matrix<-matrix(data=0, nrow=length(nodeList), ncol=2)  #this is where we will stick the model-averaged rates
+
+	for(modelIndex in 1:dim(result.iterateNonCensored)[1]) {
+		model.rates<-rep(NA,2)
+		model.rates[1]<-#result.iterateNonCensored$rate0[modelIndex] #CHECK THIS SYNTAX, make sure it works for BM1, too
+		model.rates[2]<-#result.iterateNonCensored$rate1[modelIndex] #ditto
+		AICweight<-#result.iterateNonCensored$AICweight[modelIndex]
+		BMS.node<-#result.iterateNonCensored#BMS.node[modelIndex]
+		rate.choice<-matrix(data=NA, nrow=length(nodeList), ncol=2)
+		if(result.iterateNonCensored$model[modelIndex]=="BM1") {
+			rate.choice<-matrix(data=0, nrow=length(nodeList), ncol=2)
+		}
+		else {
+			rate.choice<-getRatesAtBreak(phy,BMS.node)
+		}
+		for(nodeIndex in 1:length(nodeList)){
+			each.node<-nodeList[nodeIndex]
+			for (position in 1:2) {
+				rate.matrix[nodeIndex,position] <- rate.matrix[nodeIndex,1] + ( AICweight * model.rates[1+ rate.choice[nodeIndex,position] ] )
+			}
+		}
+	}
+	return(rate.matrix)
+}
