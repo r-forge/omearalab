bc926005b49e5ba554ffa0089007beb52fd26eea
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-06-13 08:04:35 +0000

    successful runs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@142 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/carst/run.R
+++ pkg/R/carst/run.R
@@ -2,13 +2,12 @@ setwd("/Users/bomeara/Dropbox/InProgress/Carstens/Basic")
 popVector<-rep(7,3)
 migrationArray<-generateMigrationIndividuals(popVector)
 createAssignment(popVector)
-allValues<-c(20,.2,.02,.0001,.001,.01)
+allValues<-c(5,.2,.02,.0001,.001,.01)
 names(allValues)<-c("collapse_1","n0multiplier_1","n0multiplier_2","migration_1","migration_2","migration_3")
 print(allValues)
 trueModel<-57
-nTrees=1000
+nTrees=10000
 #trueModel<-1
-library(phyclust) #just to call ms
 numModels<-length(migrationArray)
 saveMS(popVector,migrationArray[[trueModel]],allValues,nTrees=25,file="obs.tre")
 results<-data.frame()
