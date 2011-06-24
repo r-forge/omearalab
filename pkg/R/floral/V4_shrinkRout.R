57768eed266813061ae463c56d91b4eeccb6dc42
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-06-24 19:37:36 +0000

    looking for zip not Rout
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@162 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/floral/V4_shrinkRout.R
+++ pkg/R/floral/V4_shrinkRout.R
@@ -30,10 +30,10 @@ for (focalIndex in 1:length(focalVectorList)) {
 					dirRoot<-paste("/data/abc/RunsApril2011/ActualRuns/T",transitionModelIndex,"/T",transitionModelIndex,"_D",diversificationModelIndex,"/",nameRoot,sep="",collapse="")
 					lsString=paste(paste("ls -1 ",dirRoot,' | grep -c final.matrix.all',sep="",collapse=""))
 					finalMatrixAllCount=suppressWarnings(as.numeric(system(lsString,intern=TRUE)))
-					lsString=paste(paste("ls -1 ",dirRoot,' | grep -c run.Rout',sep="",collapse=""))
-					runRoutCount=suppressWarnings(as.numeric(system(lsString,intern=TRUE)))				
+					lsString=paste(paste("ls -1 ",dirRoot,' | grep -c run.zip',sep="",collapse=""))
+					runzipCount=suppressWarnings(as.numeric(system(lsString,intern=TRUE)))				
 					if(finalMatrixAllCount!=0) {
-						if (runRoutCount==1) {
+						if (runzipCount==0) {
 							system(paste("tail -40 ",dirRoot,"/run.Rout > ",dirRoot,"/tail.run.Rout",sep=""))
 							system(paste("zip ",dirRoot,"/run.zip ",dirRoot,"/run.Rout",sep=""))
 							system(paste("rm ",dirRoot,"/run.Rout",sep=""))
