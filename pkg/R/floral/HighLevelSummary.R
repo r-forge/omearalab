bc05e85f95f3d1bfdc8c71db75a50a80b6261efa
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-08-16 16:42:28 +0000

    use intermediate results for high level summary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@203 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/floral/HighLevelSummary.R
+++ pkg/R/floral/HighLevelSummary.R
@@ -4,15 +4,17 @@ ls()
 highlevel.dataframe<-data.frame()
 for (T in 1:5) {
 	for (D in 1:6) {
-		load(paste("RateSummaryT",T,"D",D,".Rsave"))
-		if (T+D==2) { #first one
-			highlevel.dataframe<-summary.dataframe[,3:13]
+		if(system(paste("ls -1 IntermediateRateSummaryT\ ",T,"\ D\ ",D,"\ .Rsave | grep -c save"),intern=TRUE)>0) {
+			load(paste("IntermediateRateSummaryT",T,"D",D,".Rsave"))
+			if (T+D==2) { #first one
+				highlevel.dataframe<-summary.dataframe[,3:13]
+			}
+			else {
+				highlevel.dataframe<-rbind(highlevel.dataframe,summary.dataframe[,3:13])
+			}
+			save(highlevel.dataframe,file="/Users/bomeara/Sites/RunsApril2011/Summaries/Highlevel.dataframe.Rsave",compress=TRUE)
+			print(paste("just did  T",T,"D",D,"with length =",dim(highlevel.dataframe)[1]))
 		}
-		else {
-			highlevel.dataframe<-rbind(highlevel.dataframe,summary.dataframe[,3:13])
-		}
-		save(highlevel.dataframe,file="/Users/bomeara/Sites/RunsApril2011/Summaries/Highlevel.dataframe.Rsave",compress=TRUE)
-		print(paste("just did  T",T,"D",D,"with length =",dim(highlevel.dataframe)[1]))
 	}
 }
 
