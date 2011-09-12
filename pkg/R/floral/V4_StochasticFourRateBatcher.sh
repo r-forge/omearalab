b396dd1e31dacca98581b53db8afc56eebe768d7
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-09-12 16:30:23 +0000

    fixed batch script
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@216 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/floral/V4_StochasticFourRateBatcher.sh
+++ pkg/R/floral/V4_StochasticFourRateBatcher.sh
@@ -6,8 +6,7 @@
 #$ -m beas
 #$ -N fourstoch
 #$ -r y
-#$ -S /bin/bash
 #$ -t 1-10
 
 module load R/2.13.0
-/data/apps/R/2.13.0/bin/R CMD BATCH ../UnifiedApproachScripts/V4_StochasticFourRateCommands.R
+/data/apps/R/2.13.0/bin/R CMD BATCH ../../UnifiedApproachScripts/V4_StochasticFourRateCommands.R
