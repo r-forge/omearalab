365c4cfaba4137108d0c8746f1e95e01b1c502e2
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2011-09-28 14:49:53 +0000

    new function still not working
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@220 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/animateBisseGoldberg.R
+++ pkg/R/animateBisseGoldberg.R
@@ -174,20 +174,16 @@ bisse.ani<-function(q0=0.0,q1=0.555,b0=5.199,b1=2.933,d0=5.433,d1=2.319,starting
     }
 }
 #
-showBisseMovie<-function() {
-  par(ann=FALSE)
+showBisseMovie<-function(q0=0.0,q1=0.555,b0=5.199,b1=2.933,d0=5.433,d1=2.319) {
   quartz()
   ani.start(nmax=1000,interval=1,title="Diversification and Character Evolution")
-  bisse.ani()
+  bisse.ani(q0=q0,q1=q1,b0=b0,b1=b1,d0=d0,d1=d1)
   ani.stop()
 }
-#
-#ani.stop()
-#
-#bisse.ani()
-saveBisseMovie<-function() {
+
+saveBisseMovie<-function(q0=0.0,q1=0.555,b0=5.199,b1=2.933,d0=5.433,d1=2.319) {
   oopt=ani.options(nmax=500,interval=0.1)
-  saveMovie(bisse.ani(),convert='convert',outdir="/Users/bomeara/Desktop/",movietype = "gif",width=600, height=600,interval=0.5)
+  saveMovie(bisse.ani(q0=q0,q1=q1,b0=b0,b1=b1,d0=d0,d1=d1),convert='convert',outdir="/Users/bomeara/Desktop/",movietype = "gif",width=600, height=600,interval=0.5)
   ani.options(oopt)
+  ani.stop()
 }
-#ani.stop()
