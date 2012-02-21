b9619e93d627bb7d8f40750bd4efdbbc1dfb2c5b
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2012-02-21 22:41:47 +0000

    cyto export
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@270 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/floral/V6_MetaSummary.R
+++ pkg/R/floral/V6_MetaSummary.R
@@ -3,6 +3,6 @@ load("Highlevel.dataframe.withrates.Rsave")
 highlevel.dataframe<-highlevel.dataframe[,c(-3,-4)] #dropping misleading columns
 best<-which.min(highlevel.dataframe$AIC)
 print(highlevel.dataframe[best,1:13])
-plot(highlevel.dataframe$k_all,highlevel.dataframe$deltaAIC+1,log="y")
+#plot(highlevel.dataframe$k_all,highlevel.dataframe$deltaAIC+1,log="y")
 print(dim(highlevel.dataframe))
 print("final dim should be 21126  4173")
