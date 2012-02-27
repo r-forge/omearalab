9b3dcf8ffe5f33141b6abb2e3a7d555b436aa1b2
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2012-02-27 21:00:05 +0000

    no boring options
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@274 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/floral/V6_ExportToCytoscapeNoBoring.R
@@ -0,0 +1,89 @@
+#this will export the correct formats for cytoscape
+source("~/Sites/RunsJan2012/UnifiedApproachScripts/V6_UtilityFns.R")
+load("~/Sites/RunsJan2012/Summaries/Highlevel.dataframe.withrates.Rsave") #replace with frame of all data
+
+bestValues<-highlevel.dataframe[which.max(highlevel.dataframe$AICweight),]
+meanValues<-bestValues
+for ( i in 11:length(meanValues) ) {
+  meanValues[i]<-weighted.mean(highlevel.dataframe[,i],highlevel.dataframe$AICweight,na.rm=TRUE)
+}
+
+#first the edges
+qValuesPositions<-grep("^q",names(meanValues))
+qDataFrame<-data.frame()
+for(i in 1:length(qValuesPositions)) {
+  position<-qValuesPositions[i]
+   if(length(grep("disallowed",names(meanValues)[position]))==0) {
+    labels<-sub("q","",strsplit(names(meanValues)[position],"_")[[1]])
+    rate<-meanValues[1,position]
+    names(rate)<-c("q")
+    if(i==1) {
+      qDataFrame<-data.frame(labels[1],labels[2],rate)
+    }
+    else {
+      qDataFrame<-rbind(qDataFrame,data.frame(labels[1],labels[2],rate))
+    }
+   }
+}
+names(qDataFrame)<-c("from","to","q")
+qDataFrame$q=qDataFrame$q/min(qDataFrame$q)
+print(qDataFrame)
+write.table(qDataFrame,file="~/Sites/RunsJan2012/Summaries/Cytoscape_edges.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=TRUE)
+
+
+#get info on number of combos
+sourcetraits="../SourceData/Stebbins_prunenoper25i2012BCO.csv"
+file<-sourcetraits
+phy<-"../SourceData/floral_1.nex"
+tree<-read.nexus(phy)
+data<-read.csv(file)
+colnamesVector<-colnames(data)
+names<-colnamesVector[2:length(colnamesVector)]
+#keep only 1 (1st 25%) and 2 (2nd 25%) in Selection column
+#subdata<-subset(data,data$Selection<=2)
+#already done, so just
+subdata<-data #to minimize recoding
+
+#delete taxa with missing data anywhere
+for (colToExamine in 2:length(colnamesVector)) {
+	subdata<-subdata[subdata[,colToExamine]!="?",]
+}
+
+comboCounts<-rep(0,2^S)
+for(i in 1:dim(subdata)[1]) {
+  currentVector<-as.numeric(unlist(subdata[i,2:7]))
+  comboDecimal<-comboAsDecimal(currentVector,S)
+  comboCounts[comboDecimal]<-comboCounts[comboDecimal]+1
+}
+comboProportions<-comboCounts/(sum(comboCounts))
+
+#nodesDataFrame<-data.frame()
+#now the nodes
+try(for (comboDecimal in 1:2^S) {
+  comboName<-comboAsBinaryString(comboDecimal,S)
+  birthRate<-as.vector(meanValues[1,which(names(meanValues)==paste("lambda",comboName,sep=""))])
+  deathRate<-as.vector(meanValues[1,which(names(meanValues)==paste("mu",comboName,sep=""))])
+  diversificationRate<-birthRate-deathRate
+  turnoverRate<-birthRate+deathRate
+  tmp.dataFrame<-data.frame(comboName,birthRate,deathRate,diversificationRate,turnoverRate,comboCounts[comboDecimal],comboProportions[comboDecimal],vectorMismatch(comboAsBinaryVector(1,S),comboAsBinaryVector(comboDecimal,S)))
+  names(tmp.dataFrame)<-c("combo","birth","death","diversification","turnover","number_taxa_with_state","proportion_taxa_with_state","steps_from_root")
+ # tmp.dataFrame<-data.frame(5)
+if (comboDecimal==1) {
+   ndf2<-tmp.dataFrame
+  }
+  else {
+    print(comboDecimal)
+    print(tmp.dataFrame)
+    ndf2<-rbind(ndf2,tmp.dataFrame)
+    rm(tmp.dataFrame)
+    print("done rbinding")
+  }
+  print(dim(ndf2))
+  ndf3<-ndf2
+  print("done if else")
+})
+print("done combo decimal")
+print(ndf3)
+write.table(ndf3,file="~/Sites/RunsJan2012/Summaries/Cytoscape_nodes.txt",quote=FALSE,sep="\t",row.names=FALSE,col.names=TRUE)
+
+print(subdata)
--- pkg/R/floral/V6_MetaSummary.R
+++ pkg/R/floral/V6_MetaSummary.R
@@ -6,3 +6,51 @@ print(highlevel.dataframe[best,1:13])
 #plot(highlevel.dataframe$k_all,highlevel.dataframe$deltaAIC+1,log="y")
 print(dim(highlevel.dataframe))
 print("final dim should be 21126  4173")
+print(highlevel.dataframe[which(highlevel.dataframe$deltaAIC<10),1:13])
+
+
+print(unique(c(highlevel.dataframe[best,14:dim(highlevel.dataframe)[2] ])))
+
+printUniqueValues<-function(focalRow) {
+   combo<-focalRow$focal
+   focalRow.unnamed<-as.vector(focalRow)
+   names(focalRow.unnamed)<-NULL
+   combo.xto0<-gsub("x","0",combo)
+   combo.mostxto0<-gsub("x","0",sub("x","1",combo)) #change first x to 1, others to 0
+   combo.mismatch<-""
+   if (length(grep("0",combo))>0) { #has a zero to move
+    combo.mismatch<-sub("0","1",combo) #won't match combo
+   }
+   else { #has a 1 to move
+    combo.mismatch<-sub("1","0",combo) #won't match combo
+     
+   }
+   combo.mismatch.xto0<-gsub("x","0",combo.mismatch)
+   combo.mismatch.mostxto0<-gsub("x","0",sub("x","1",combo.mismatch))
+   print(c(combo,combo.xto0, combo.mostxto0, combo.mismatch, combo.mismatch.xto0, combo.mismatch.mostxto0))
+   #now get matches and mismatches: q_combo.xto0_combo.mismatch.xto0 is focal to nonfocal
+   rates<-rep(NA,10)
+   names(rates)<-c("lambdaF","muF","divF","lambdaN","muN","divN","qNF","qFN","qNN","qFF")
+   try(rates[1]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("lambda",combo.xto0,sep=""))][[1]]))
+   try(rates[2]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("mu",combo.xto0,sep=""))][[1]]))
+   try(rates[3]<-rates[1]-rates[2])
+   try(rates[4]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("lambda",combo.mismatch.xto0,sep=""))][[1]]))
+   try(rates[5]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("mu",combo.mismatch.xto0,sep=""))][[1]]))
+   try(rates[6]<-rates[4]-rates[5])
+   try(rates[7]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.mismatch.xto0,"_",combo.xto0,sep=""))][[1]]))
+   try(rates[8]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.xto0,"_",combo.mismatch.xto0,sep=""))][[1]]))
+   try(rates[9]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.mismatch.xto0,"_",combo.mismatch.mostxto0,sep=""))][[1]]))
+   try(rates[10]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.xto0,"_",combo.mostxto0,sep=""))][[1]]))
+   print(rates)
+   return(rates)
+}
+
+bestrows<-which(highlevel.dataframe$deltaAIC<10)
+bestsummary.df<-data.frame(c(highlevel.dataframe[best,1:13],printUniqueValues(highlevel.dataframe[best,])),stringsAsFactors=FALSE)
+for (i in sequence(length(bestrows))) {
+  print(i)
+ try(bestsummary.df<-rbind(bestsummary.df,(c(highlevel.dataframe[bestrows[i],1:13],printUniqueValues(highlevel.dataframe[bestrows[i],])))))
+}
+bestsummary.df<-bestsummary.df[-1,]
+bestsummary.df
+write.csv(bestsummary.df,file="bestsummary.csv")
