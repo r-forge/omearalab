3d59230ed7c1e872081c6ab9a12bd5aba9f75563
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2012-08-10 20:32:05 +0000

    new simulator
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/floral/V6_NewSimulator.R
@@ -0,0 +1,90 @@
+library(RColorBrewer)
+
+OMearaSSA<-function(x0, q.vector, lambda.vector, mu.vector, tf, maxWallTime, verbose=TRUE) {
+  #make sure order of states is the same in all input objects
+  q.matrix<-matrix(0,nrow=length(x0),ncol=length(x0))
+  rownames(q.matrix)<-names(x0)
+  colnames(q.matrix)<-names(x0)
+  for (i in sequence(length(q.vector))) {
+    q.matrix[ which(rownames(q.matrix)==extractFrom(names(q.vector)[i])), 
+              which(colnames(q.matrix)==extractTo(names(q.vector)[i]))
+              ]<-q.vector[i]
+  }
+  names(lambda.vector)<-gsub("lambda","",names(lambda.vector))
+  names(mu.vector)<-gsub("mu","",names(mu.vector))
+  #print(q.matrix)
+  all.matrix<-cbind(q.matrix,matrix(lambda.vector,ncol=1),matrix(mu.vector,ncol=1))
+  #print(all.matrix)
+  start.time<-Sys.time()
+  x<-x0
+  history<-matrix(c(0,x0),nrow=1)
+  while ((Sys.time()-start.time)<maxWallTime) {
+    rate.matrix<-ScaleTransitionProbs(all.matrix,x)
+    time.interval<-rexp(1, sum(rate.matrix))
+    if ((sum(history[,1])+time.interval)>tf) {
+      history<-rbind(history,c(tf-sum(history[,1]),x)) #done running
+      return(history)
+    }
+    change.row<-which(rmultinom(1,1,apply(rate.matrix, 1, sum))==1)
+    change.col<-which(rmultinom(1,1,rate.matrix[change.row,])==1)
+    move<-""
+    if(change.col<=length(x)) { #transition
+      x[change.row]<-x[change.row]-1
+      x[change.col]<-x[change.col]+1
+      if (verbose) {
+         move<-paste("t_",names(x0[change.row]),"->",names(x0[change.col]),sep="")
+      }
+    }
+    if(change.col==(length(x)+1)) { #birth
+      x[change.row]<-x[change.row]+1
+      if (verbose) {
+        move<-paste("b_",names(x0[change.row]),sep="")
+      }
+    }
+    if(change.col==(length(x)+2)) { #death
+      x[change.row]<-x[change.row]-1
+      if (verbose) {
+        move<-paste("d_",names(x0[change.row]),sep="")
+      }
+    }
+
+    history<-rbind(history,c(time.interval,x))
+    if(verbose) {
+     print(paste(sum(x),paste(x,sep=" ",collapse=" "),time.interval, sum(history[,1]), move, sep=" "))
+    }
+    if(sum(x)==0) {
+      if(verbose) {
+        print("all extinct")
+      }
+     return(history) 
+    }
+  }
+  return(history)
+}
+
+ScaleTransitionProbs<-function(all.matrix, x) {
+  return(all.matrix*x) #this multiplies each element in the i-th row of the matrix by the i-th element of x.  
+}
+
+plot.OMearaSSA<-function(history) {
+  ncombos<-dim(history)[2]-1
+  mypalette<-brewer.pal(ncombos,"Dark2")
+ plot(range(cumsum(history[,1])),range(history[,2:dim(history)[2] ]),xlab="time",ylab="ntaxa",type="n",bty="n") 
+ for (i in 2:dim(history)[2]) {
+  lines( cumsum(history[,1]), history[, i],col=mypalette[i-1])
+ }
+  legend(x="topleft",legend=colnames(history)[2:(ncombos+1)], text.col=mypalette)
+}
+  
+
+
+survivors<-0
+history<-0
+attempts<-0
+while(survivors<=0) {
+  attempts<-attempts+1
+  history<-OMearaSSA(x0,q.means,lambda.means,mu.means,tf=30,maxWallTime=10,verbose=T)
+  survivors<-history[dim(history)[1],2]
+}
+
+plot.OMearaSSA(history)
--- pkg/R/floral/V6_StochasticSSASims.R
+++ pkg/R/floral/V6_StochasticSSASims.R
@@ -7,6 +7,8 @@ highlevel.dataframe$AICweight<-exp( -0.5 * highlevel.dataframe$deltaAIC)
 highlevel.dataframe$AICweight <- highlevel.dataframe$AICweight/sum(highlevel.dataframe$AICweight)
 highlevel.dataframe<-highlevel.dataframe[order(highlevel.dataframe$AICweight, decreasing=TRUE),]
 
+source("/Users/bomeara/Documents/MyDocuments/Active/OMearaLabR/pkg/R/floral/V6_UtilityFns.R")
+
 key.focal.vector<-c("0x00xx","0x01xx","0x10xx","0x11xx","1x00xx","1x01xx","1x10xx","1x11xx")
 
 getMatchingTransitions<-function(focalRow, key.focal.from, key.focal.to, change.positions=c(2, 5, 6), average=TRUE) {
@@ -37,40 +39,68 @@ getMatchingTransitions<-function(focalRow, key.focal.from, key.focal.to, change.
   }
 }
 
-q.values<-matrix(nrow=dim(highlevel.dataframe)[1],ncol=24)
+getMatchingTransitionIndices<-function(name.vector, key.focal.from, key.focal.to, change.positions=c(2, 5, 6)) {
+  key.focal.from.split<-strsplit(key.focal.from, "")[[1]]
+  key.focal.to.split<-strsplit(key.focal.to, "")[[1]]
+  matching.indices<-c()
+  for (i in c(0:1)) {
+    key.focal.from.split[change.positions[1]]<-i
+    key.focal.to.split[change.positions[1]]<-i   
+    for (j in c(0:1)) {
+      key.focal.from.split[change.positions[2]]<-j
+      key.focal.to.split[change.positions[2]]<-j
+      for (k in c(0:1)) {
+        key.focal.from.split[change.positions[3]]<-k
+        key.focal.to.split[change.positions[3]]<-k
+        #print(names(focalRow)[1:25])
+        #print(paste("q", paste(key.focal.from.split, sep="", collapse=""), "_", paste(key.focal.to.split, sep="", collapse=""), sep=""))
+        matching.indices<-append(matching.indices,which(name.vector==paste("q", paste(key.focal.from.split, sep="", collapse=""), "_", paste(key.focal.to.split, sep="", collapse=""), sep="")))
+      }
+    }
+  }
+  return(matching.indices)
+  
+}
+
+focal.dataframe<-highlevel.dataframe[,]
+
+q.values<-matrix(nrow=dim(focal.dataframe)[1],ncol=24)
 q.names<-c()
 for (focal.index.1 in sequence(length(key.focal.vector))) {
-  for (focal.index.2 in c(focal.index.1:length(key.focal.vector))) {
+  for (focal.index.2 in sequence(length(key.focal.vector))) {
     if (focal.index.1 != focal.index.2) {
+      print(c(focal.index.1,focal.index.2))
       if( vectorMismatch(unlist(strsplit(key.focal.vector[focal.index.1],split="")), unlist(strsplit(key.focal.vector[focal.index.2], split="")))==1) {
         q.names<-append(q.names,paste("q", key.focal.vector[focal.index.1], "_", key.focal.vector[focal.index.2], sep=""))
         #print(paste("q", key.focal.vector[focal.index.1], "_", key.focal.vector[focal.index.2], sep=""))
-        for (element in sequence(dim(highlevel.dataframe)[1])) {
-          q.values[element, length(q.names)]<-getMatchingTransitions(highlevel.dataframe[1,], key.focal.vector[focal.index.1], key.focal.vector[focal.index.2])
-        }
-       # print(getMatchingTransitions(highlevel.dataframe[1,], key.focal.vector[focal.index.1], key.focal.vector[focal.index.2]))
-        #print(apply(highlevel.dataframe[1:10,], 1, getMatchingTransitions, key.focal.from = key.focal.vector[focal.index.1], key.focal.to = key.focal.vector[focal.index.2] ))
-        #q.values<-cbind(q.values,data.frame(matrix(apply(highlevel.dataframe[1:10,], 1, getMatchingTransitions, key.focal.from = key.focal.vector[focal.index.1], key.focal.to = key.focal.vector[focal.index.2] ) , ncol=1)))
+        matching.indices<-getMatchingTransitionIndices(names(focal.dataframe),  key.focal.vector[focal.index.1], key.focal.vector[focal.index.2])
+        q.values[,length(q.names)] <- apply(focal.dataframe[,matching.indices], 1, mean)
       }
     }
   }
 }
 
-#do matching like that below to get all the columns for q01001_01000 and so forth. Average the relevant columns for each row, 
-#  then do weighted average of these across the rows based on AIC weights
-#  for doing models with equal diversification, filter for those models that have the simplest diversification model and redo.
 
-#grepl(key.focal.xtoperiod[2],names(highlevel.dataframe),perl=TRUE)
+colnames(q.values)<-q.names
 
-
-
-
-
-
-
-
+lambda.values<-matrix(nrow=dim(focal.dataframe)[1],ncol=8)
+lambda.names<-c()
+for (focal.index.1 in sequence(length(key.focal.vector))) {
+  lambda.names<-append(lambda.names, paste("lambda",key.focal.vector[focal.index.1],sep=""))
+  matching.indices <- grepl(gsub("x",".",paste("lambda",key.focal.vector[focal.index.1],sep="")), names(focal.dataframe), perl=TRUE)
+  lambda.values[, length(lambda.names)] <- apply(focal.dataframe[,matching.indices], 1, mean)
+}
+colnames(lambda.values)<-lambda.names
 
 
+mu.values<-matrix(nrow=dim(focal.dataframe)[1],ncol=8)
+mu.names<-c()
+for (focal.index.1 in sequence(length(key.focal.vector))) {
+  mu.names<-append(mu.names, paste("mu",key.focal.vector[focal.index.1],sep=""))
+  matching.indices <- grepl(gsub("x",".",paste("mu",key.focal.vector[focal.index.1],sep="")), names(focal.dataframe), perl=TRUE)
+  mu.values[, length(mu.names)] <- apply(focal.dataframe[,matching.indices], 1, mean)
+}
+colnames(mu.values)<-mu.names
 
 
 
@@ -78,176 +108,94 @@ for (focal.index.1 in sequence(length(key.focal.vector))) {
 
 
 
-printUniqueValues<-function(focalRow) {
-  combo<-focalRow[which(names(focalRow)=="focal")]
-  print(combo)
-  focalRow.unnamed<-as.vector(focalRow)
-  names(focalRow.unnamed)<-NULL
-  combo.xto0<-gsub("x","0",combo)
-  combo.mostxto0<-gsub("x","0",sub("x","1",combo)) #change first x to 1, others to 0
-  combo.mismatch<-""
-  if (length(grep("0",combo))>0) { #has a zero to move
-    combo.mismatch<-sub("0","1",combo) #won't match combo
-  }
-  else { #has a 1 to move
-    combo.mismatch<-sub("1","0",combo) #won't match combo
-    
-  }
-  combo.mismatch.xto0<-gsub("x","0",combo.mismatch)
-  combo.mismatch.mostxto0<-gsub("x","0",sub("x","1",combo.mismatch))
-#  print(c(combo,combo.xto0, combo.mostxto0, combo.mismatch, combo.mismatch.xto0, combo.mismatch.mostxto0))
-  #now get matches and mismatches: q_combo.xto0_combo.mismatch.xto0 is focal to nonfocal
-  rates<-rep(NA,10)
-  names(rates)<-c("lambdaF","muF","divF","lambdaN","muN","divN","qNF","qFN","qNN","qFF")
-  try(rates[1]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("lambda",combo.xto0,sep=""))][[1]]))
-  try(rates[2]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("mu",combo.xto0,sep=""))][[1]]))
-  try(rates[3]<-rates[1]-rates[2])
-  try(rates[4]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("lambda",combo.mismatch.xto0,sep=""))][[1]]))
-  try(rates[5]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("mu",combo.mismatch.xto0,sep=""))][[1]]))
-  try(rates[6]<-rates[4]-rates[5])
-  try(rates[7]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.mismatch.xto0,"_",combo.xto0,sep=""))][[1]]))
-  try(rates[8]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.xto0,"_",combo.mismatch.xto0,sep=""))][[1]]))
-  try(rates[9]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.mismatch.xto0,"_",combo.mismatch.mostxto0,sep=""))][[1]]))
-  try(rates[10]<-as.numeric(focalRow.unnamed[which(names(focalRow)==paste("q",combo.xto0,"_",combo.mostxto0,sep=""))][[1]]))
-  #print(rates)
-  return(rates)
+
+
+
+q.means <- apply(q.values, 2, weighted.mean, w=focal.dataframe$AICweight)
+lambda.means <- apply(lambda.values, 2, weighted.mean, w=focal.dataframe$AICweight)
+mu.means <- apply(mu.values, 2, weighted.mean, w=focal.dataframe$AICweight)
+
+#go Yule just to try
+yule.scale<-0 #if 0, no different from regular birth death; if 1, pure birth
+lambda.means<-lambda.means-(yule.scale*mu.means)
+mu.means<-mu.means*(1-yule.scale)
+
+x0 <- c(2, 0, 0, 0, 0, 0, 0, 0) #start with two individuals in state 000000, or at least 0x00xx
+names(x0) <- key.focal.vector
+
+parms<-c(q.means, lambda.means, mu.means)
+names(parms)
+
+extractFromAndPaste<-function(transition.name) {
+  m <- regexpr("q(.+)\\_",transition.name, perl=F)
+   return(paste(sub("_","",sub("q","",regmatches(transition.name, m))), transition.name, sep="*"))
 }
 
-
-unique.rates<-t(apply(highlevel.dataframe,1,printUniqueValues))
-
-
-
-oneSim<-function(maxWallTime=Inf) {
- taskid<-Sys.getenv("SGE_TASK_ID")
-  starttime<-Sys.time()
-  x0<-c(state00=2,state01=0,state11=0,state10=0)
-
-  qNN=0.00143651
-  qNF=0.00600116
-  qFN=0.00143651
-  qFF=0.00143651
-  birthN=5.893707
-  deathN=5.856925
-  birthF=2.03713
-  deathF=2.0089
-  
-  #00=ancestor, 01=focal == xxx0xx1x
-  parms<-c(
-   q00_01=qNF,
-   q00_10=qNN,
-   q01_11=qFN,
-   q01_00=qFN,
-   q11_10=qNN,
-   q11_01=qNF,
-   q10_11=qNN,
-   q10_00=qNN,
-   birth00=birthN,
-   birth01=birthF,
-   birth11=birthN,
-   birth10=birthN,
-   death00=deathN,
-   death01=deathF,
-   death11=deathN,
-   death10=deathN
-  )
- 
-
-
-  aNames<-c(
-    "00 to 01",
-    "00 to 10",
-    "01 to 00",
-    "01 to 11",
-    "11 to 01",
-    "11 to 10",
-    "10 to 11",
-    "10 to 00",
-    "birth00",
-    "birth01",
-    "birth11",
-    "birth10",
-    "death00",
-    "death01",
-    "death11",
-    "death10"
-  )
-  
-  a<-c(
-      "state00*q00_01",
-      "state00*q00_10",
-      "state01*q01_00",
-      "state01*q01_11",
-      "state11*q11_01",
-      "state11*q11_10",
-      "state10*q10_11",
-      "state10*q10_00",
-      "state00*birth00",
-      "state01*birth01",
-      "state11*birth11",
-      "state10*birth10",
-      "state00*death00",
-      "state01*death01",
-      "state11*death11",
-      "state10*death10"
-  )
-  
- #state order is 
-#00
-#01
-#11
-#10
-  nu<-matrix(c(
-    -1,1,0,0,
-    -1,0,1,0,
-    1,-1,0,0,
-    0,-1,1,0,
-    0,1,-1,0,
-    0,0,-1,1,
-    0,0,1,-1,
-    1,0,0,-1,
-    1,0,0,0,
-    0,1,0,0,
-    0,0,1,0,
-    0,0,0,1,
-    -1,0,0,0,
-    0,-1,0,0,
-    0,0,-1,0,
-    0,0,0,-1
-    ), byrow=FALSE,nrow=length(x0))
-    
-  #a has reactions: the possible step moves
- # aNames<-   c("F to N","N to F","F birth", "F death", "N birth", "N death")
-#  a<-        c("F*qFN", "N*qNF", "F*birthF","F*deathF","N*birthN","N*deathN")
-  #nu<-matrix(c(-1,1,     1,-1,    1,0,       -1,0,      0,1,       0,-1),byrow=FALSE,nrow=length(x0)) #rows are states (F,N) and cols are the processes
-
-  out<-ssa(x0,a,nu,parms,tf=130,maxWallTime=maxWallTime,method="OTL")
-  outvector<-c(as.numeric(Sys.time()-starttime,units="secs"),out$data[dim(out$data)[1],])
-  names(outvector)[1]<-"CPUtime"
-  names(outvector)[2]<-"Duration"
-  cat(outvector,"\n",file=paste("StochasticFourRate_",taskid,".txt",sep=""),append=TRUE)
- # ssa.plot(out)
-  return(outvector)
+extractFrom<-function(transition.name) {
+  m <- regexpr("q(.+)\\_",transition.name, perl=F)
+  return(sub("_","",sub("q","",regmatches(transition.name, m))))
 }
 
-#IMPORTANT:
-#The way the batching happens, each set of runs will end with a long run. So to estimate
-#the proportion of sims that go extinct, you'll have to take this into account (something like
-#using the wait time until a success to estimate the probability, rather than just taking the
-#ratio of number of runs that ended up with species to the total number of runs). Another way 
-#would be to calculate the raw ratio, but using just the first X simulations in each run, where
-#X is the minimum number of simulations in any run
+extractTo<-function(transition.name) {
+  m <- regexpr("\\_(.+)",transition.name, perl=F)
+  return(sub("_","",regmatches(transition.name, m)))
+}
 
+a<-sapply(names(q.means), extractFromAndPaste)
+a<-append(a, apply(cbind(key.focal.vector,names(lambda.means)), 1, paste, sep="", collapse="*"))
+a<-append(a, apply(cbind(key.focal.vector,names(mu.means)), 1, paste, sep="", collapse="*"))
+names(a)<-NULL
 
-taskid<-Sys.getenv("SGE_TASK_ID")
-overallstarttime<-Sys.time()
-sims<-cbind(oneSim())
-save(sims,file=paste("StochasticFourRate_",taskid,".RSave",sep=""),compress=TRUE)
-minduration<-3600 #one hour
-while(as.numeric(Sys.time()-overallstarttime,units="secs")<minduration) {
-  sims2<- cbind(oneSim())
-  if(!is.null(dim(sims2))) {
-     sims<-cbind(sims,sims2)
-     save(sims,file=paste("StochasticFourRate_",taskid,".RSave",sep=""),compress=TRUE)
+#states are rows, transitions are columns
+nu<-matrix(0,nrow=length(x0),ncol=length(a))
+for (i in sequence(length(a))) {
+  if(grepl("lambda",a[i])) {
+     combo<-substr(a[i],1,6)
+     nu[which(names(x0)==combo),i]<-1
   }
+  if(grepl("mu",a[i])) {
+     combo<-substr(a[i],1,6)
+     nu[which(names(x0)==combo),i]<- (-1)
+  }
+  if(grepl("q",a[i])) {
+    nu[which(names(x0)==extractFrom(a[i])),i]<- (-1)
+    nu[which(names(x0)==extractTo(a[i])),i]<- (1)
+  }
+}
+
+#now to get rid of the x, which the ssa fn does not like
+
+
+
+trySim<-function(x0, a, nu, parms, tf=130, maxWallTime=10) {
+  names(parms)<-gsub("x","",names(parms))
+  names(x0)<-gsub("x","",names(x0))
+  a<-gsub("x","",a)
+  min.val<-(-1)
+  out<-c()
+  try.count<-0
+while (min.val<0) {
+  try.count<-try.count+1
+  print(try.count)
+  out<-ssa(x0,a,nu,parms,tf=tf,maxWallTime=maxWallTime,method="D",ignoreNegativeState=TRUE)
+  min.val<-min(out$data)
+    ssa.plot(out)
+  print(c(max(out$data[,1]),max(apply(out$data[,2:9],1, sum))))
+
+  #plot(out$data[,1],apply(out$data[,2:9],1, sum))
+  }
+  print(tail(out$data))
+  ssa.plot(out)
+  return(out)
+}
+
+out<-trySim(x0, a, nu, parms, tf=130)
+
+probExtinction<-function(lambda, mu, t, N0=2) {
+  #after magallon and sanderson
+  epsilon<-mu/lambda
+  r<-lambda-mu
+  beta<-(exp(r*t) - 1) / (exp(r*t) - epsilon)
+  alpha<-epsilon * beta
+  return(alpha^N0)
 }
