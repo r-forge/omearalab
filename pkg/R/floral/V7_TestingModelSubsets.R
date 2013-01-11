5dc63b5789d75f6a8183948ba790e5d9a9fe3d98
bcomeara (Brian O'Meara) <omeara.brian@gmail.com>
2013-01-11 22:04:13 +0000

    AIC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/omearalab@341 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 9c46151291e4d13c93c40e77811259fcea345ec1

--- pkg/R/floral/V7_TestingModelSubsets.R
+++ pkg/R/floral/V7_TestingModelSubsets.R
@@ -1,6 +1,6 @@
-source("V6_UtilityFns.R")
-load("all.results.cleaned.Rsave")
+source("/Users/bomeara/Documents/MyDocuments/Active/OMearaLabR/pkg/R/floral/V6_UtilityFns.R")
+load("/Users/bomeara/Documents/MyDocuments/Active/FloralAssembly/RunsJan2012/Summaries/all.results.cleaned.Rsave")
 testing.model.subset<-highlevel.dataframe[c(1:50, round(seq(from=100, to=19600, length.out=50))),] #100 models used for evaluating reproducibility, AIC weights
 testing.model.subset$AICweight<-testing.model.subset$AICweight/sum(testing.model.subset$AICweight)
-setwd("/Users/bomeara/Documents/MyDocuments/Active/FloralAssembly/AICweightJan2012")
+setwd("/Users/bomeara/Documents/MyDocuments/Active/FloralAssembly/AICweightJan2013")
 save(testing.model.subset, file="testing.model.subset.Rsave", compress=TRUE)
